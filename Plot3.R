4e4553b1a2b387f79fdc8fd29e2b867943137d50
Eric Torti <ericzumba@gmail.com>
2014-08-09 20:39:20 -0300

    Plot 3 with legend

--- Plot3.R
+++ Plot3.R
@@ -9,6 +9,12 @@ plot(x$DateTime,
      ylab = "Energy sub metering",
      xlab = "",
      type="n")
+
+legend("topright",
+       c("Sub metering 1", "Sub metering 2", "Sub metering 3"), 
+       lty = c(1, 1, 1),
+       col = c("black", "red", "blue"))
+
 lines(x$DateTime, x$Sub_metering_1)
 par(col="red")
 lines(x$DateTime, x$Sub_metering_2)
