14516b691e243bb2375dad9a413b7f6f79e4e514
Eric Torti <ericzumba@gmail.com>
2014-08-09 01:16:42 -0300

    sub(pimba, x)

--- Plot1.R
+++ Plot1.R
@@ -1,8 +1,8 @@
-pimba <- load("household_power_consumption.txt")
-pimba$Global_active_power <- as.numeric(pimba$Global_active_power)
+x <- load("household_power_consumption.txt")
+x$Global_active_power <- as.numeric(x$Global_active_power)
 png("plot1.png")
 hist(
-  (pimba$Global_active_power / 500), 
+  (x$Global_active_power / 500), 
   breaks = 15, 
   xlab = "Global Active Power (kilowatts)", 
   col = "red", 
