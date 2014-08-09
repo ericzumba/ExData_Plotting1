17a62349b6e48d19558730e4d90772b304ed5b93
Eric Torti <ericzumba@gmail.com>
2014-08-09 20:07:50 -0300

    Fixing scales on Plot1 and Plot2

--- Plot1.R
+++ Plot1.R
@@ -2,7 +2,7 @@ x <- loadCached(cacheable, "household_power_consumption.txt")
 x$Global_active_power <- as.numeric(x$Global_active_power)
 # png("plot1.png")
 hist(
-  (x$Global_active_power / 500), 
+  (x$Global_active_power), 
   breaks = 15, 
   xlab = "Global Active Power (kilowatts)", 
   col = "red", 
--- Plot2.R
+++ Plot2.R
@@ -2,10 +2,10 @@ x <- loadCached(cacheable, "household_power_consumption.txt")
 x$Global_active_power <- as.numeric(x$Global_active_power)
 
 x$DateTime <- as.POSIXct(paste(x$Date, x$Time), format="%d/%m/%Y %H:%M:%S")
-png(filename="plot2.png")
+# png(filename="plot2.png")
 plot(x$DateTime, 
-     (x$Global_active_power / 500), type="l", 
+     (x$Global_active_power), type="l", 
      ylab = "Global Active Power (kilowatts)",
      xlab = ""
 )
-dev.off()
+# dev.off()
